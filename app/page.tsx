a19703a5ce312f35b048dbade66131d677c6321f
1ui50M <delontiverosm@gmail.com>
2025-04-10 20:55:18 -0400

    Probando las APIs

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
 import Card from "@/components/Card";
-export default function Home() {
+
+export default async function Home() {
+  const data = await fetch('https://api.thecatapi.com/v1/images/search');
+  const posts = await data.json();
+
+  const cat = await fetch('https://api.thecatapi.com/v1/images/0XYvRd7oD');
+  const catData = await cat.json();
+
+  console.log("data", catData);
+
   return (
     <>
-    <Card/>
-    <Card/>
+      <Card cardData={catData} />
     </>
   );
 }
+
--- components/Card.tsx
+++ components/Card.tsx
@@ -2,9 +2,17 @@ import Image from "next/image"
 import Header from "./Header"
 import Blog from "./Blog"
 import Profile from "./Profile"
-
-const Card = () => {
-  return (
+interface CardProps{
+  cardData: {
+    id : string;
+    url : string;
+    width : number;
+    height : number;
+  };
+}
+const Card = async(props :CardProps) => {
+  console.log(props.cardData);
+return (
     <div className="contenido">
         <div className="parte-1">
         <Image src={"./illustration-article.svg"} alt={""} width={320} height={100}/>
